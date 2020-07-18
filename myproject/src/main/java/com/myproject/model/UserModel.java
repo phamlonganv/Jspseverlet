6f222377a42c4dc05e1cf09e475c973d5709cfb3
Phạm Long Ẩn <anplps09463@fpt.edu.vn>
2020-07-18 14:02:02 +0700

    l

--- myproject/src/main/java/com/myproject/model/UserModel.java
+++ myproject/src/main/java/com/myproject/model/UserModel.java
@@ -1,14 +1,16 @@
 package com.myproject.model;
 
-public class UserModel extends AbstractModel<UserModel> {
-	
+public class UserModel {
 	private String userName;
 	private String fullName;
-	private String password;
-	private int status;
-	private Long roleId;
-	private RoleModel role = new RoleModel();
 	
+	public UserModel() {};
+	
+	public UserModel(String userName, String fullName) {
+		super();
+		this.userName = userName;
+		this.fullName = fullName;
+	}
 	public String getUserName() {
 		return userName;
 	}
@@ -21,28 +23,5 @@ public class UserModel extends AbstractModel<UserModel> {
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getStatus() {
-		return status;
-	}
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	public Long getRoleId() {
-		return roleId;
-	}
-	public void setRoleId(Long roleId) {
-		this.roleId = roleId;
-	}
-	public RoleModel getRole() {
-		return role;
-	}
-	public void setRole(RoleModel role) {
-		this.role = role;
-	}
+	
 }
